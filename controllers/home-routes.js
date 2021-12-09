c20b1036e0ba950cf85cfc79520cfdae4b64d975
Travis Lloyd <travis.t.lloyd1996@gmail.com>
2021-12-08 22:24:58 -0700

    added conditional rendering

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -33,7 +33,58 @@ router.get('/', (req, res) => {
             //needed to get all posts, not just one. and serializes it so you dont just get a sequalize object.
             const posts = dbPostData.map(post => post.get({ plain: true }));
             // second argument is what you will be rendering into the file (first arg), homepage is what handlebards file we are rendersing.
-            res.render('homepage', {posts});
+            res.render('homepage', {
+                posts,
+                loggedIn: req.session.loggedIn
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+//single post render
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+
+            // serialize the data
+            const post = dbPostData.get({ plain: true });
+
+            res.render('single-post', {
+                post,
+                loggedIn: req.session.loggedIn
+            });
         })
         .catch(err => {
             console.log(err);
@@ -44,11 +95,11 @@ router.get('/', (req, res) => {
 //route for generating login
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
-      res.redirect('/');
-      return;
+        res.redirect('/');
+        return;
     }
-  
+
     res.render('login');
-  });
+});
 
 module.exports = router;
